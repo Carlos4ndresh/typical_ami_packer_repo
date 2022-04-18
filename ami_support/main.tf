a8aa9294324bcfd03f11a6cb893c8edca5ba6922
Carlos Andrés Herrera Pérez <29789062+Carlos4ndresh@users.noreply.github.com>
2022-04-17 22:18:17 -0300

    adding lambda support

--- ami_support/main.tf
+++ ami_support/main.tf
@@ -5,3 +5,32 @@ resource "aws_ssm_parameter" "latest_ami_id" {
   # This will change once the pipeline stage is approved
   value = "ami-xxxxxx"
 }
+
+data "aws_iam_policy_document" "lambda_ami_assume_policy_template" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_policy" "lambda_role_policy" {
+  name        = "Lambda_test_Policy"
+  description = "Policy for lambda function that will bring up and down a test EC2 with the new AMI"
+  policy      = ""
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name               = "ami-lambda-test"
+  assume_role_policy = data.aws_iam_policy_document.lambda_ami_assume_policy_template.json
+}
+
+resource "aws_lambda_function" "init_test_ec2" {
+  function_name = ""
+  architectures = ["x86_64"]
+  role          = aws_iam_role.lambda_role.arn
+}
